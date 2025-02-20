3e0484783167211606a85796b0f8b835e085e3b1
ideaswonderland <leventaydin.ce@gmail.com>
2025-02-20 15:28:37 +0300

    MEM Telefon eklendi

--- mySplit.py
+++ mySplit.py
@@ -33,6 +33,9 @@ class Fatura():
         #Telefon dosya yolları
         self.yolFatura_T = r'%s\Telefon\Fatura Listesi.xlsx'%(dosyaYolu)
         self.yolMYS_T = r'%s\Telefon\MYS.xlsx'%(dosyaYolu)  
+        #MEM Telefon dosya yolları
+        self.yolFatura_MT = r'%s\MEM\Telefon\Fatura Listesi.xlsx'%(dosyaYolu)
+        self.yolMYS_MT = r'%s\MEM\Telefon\MYS.xlsx'%(dosyaYolu)  
         #Doğalgaz dosya yolları      
         self.yolFatura_D = r'%s\Doğalgaz\Fatura Listesi.xlsx'%(dosyaYolu)
         self.yolMYS_D = r'%s\Doğalgaz\MYS.xlsx'%(dosyaYolu)  
@@ -684,6 +687,140 @@ class Fatura():
                     'memTertip': memTertip
                 }       
 
+
+                MYS(df_mys,df_mebbis,df_mebbis_dummy, df_kurum_ana, df_kurum_mem, firma, faturaTür, imzaListe, dosyaYolu, tekKaynak, harcamaTalimatı).MYS()
+
+    def MEMTelefon(self):
+        yolFatura = self.yolFatura_MT
+        yolMYS = self.yolMYS_MT
+        yolKurum = self.yolKurum
+        yolİmza = self.yolİmza
+
+        df_firma = pd.read_excel(io='firma.xlsx',sheet_name='Telefon', header=0)
+
+        firma = df_firma.values[0][1]
+        faturaTür = 'Telefon Aboneliği Ödemesi'
+
+        warnings.simplefilter(action='ignore', category=UserWarning)
+        
+        if os.path.exists(yolFatura):
+            if os.path.exists(yolMYS):
+                df_imza = pd.read_excel(yolİmza, header=0) #mys'den indirilen dosyayı okuyor
+                imzaListe = [
+                    df_imza.values[0][0],
+                    df_imza.values[1][0],
+                    df_imza.values[0][1],
+                    df_imza.values[1][1]
+                ]
+
+                df_mys = pd.read_excel(yolMYS, header=0) #mys'den indirilen dosyayı okuyor
+                cols_to_keep_mys = [
+                    'Fatura No',
+                    'Harcama Birimi',
+                    'Fatura Tarihi',
+                    'Ödenecek Tutar',
+                    'Müşteri Kimlik Bilgisi'
+                ]
+                df_mys = df_mys.reindex(cols_to_keep_mys, axis=1) #mys dosyasından verileri çekiyor
+                df_mys[['VKN', 'Okul']] = df_mys['Harcama Birimi'].str.split('-', expand=True) #Harcama Birimi kısmını VKN ve Okul olarak ayırıyor
+                df_mys[['Tarih', 'Boş']] = df_mys['Fatura Tarihi'].str.split(' ', expand=True) #Fatura tarihi kısımını ayırıyor
+                df_mys['Tarih'] = df_mys['Tarih'].str.replace('-', '/') #Fatura tarihini mebbis'ten alınan dosyanın formatına dönüştürüyor
+                df_mys[['Hizmet No','Abone']]=df_mys['Müşteri Kimlik Bilgisi'].str.split('-', expand=True) #Müşteri kimlik bilgisi kısmını bölüyor
+                df_mys['Abone'] = df_mys['Abone'].str.replace(" ","")
+                df_mys['Abone']=df_mys['Abone'].astype(float) #abone numaralarını mebbis dosyasıyla eşlemek için tam sayı haline getiriyor
+                #gereksiz sütunları temizliyor
+                df_mys = df_mys.drop(columns=['Harcama Birimi',
+                                            'Fatura Tarihi',
+                                            'Boş',
+                                            'Müşteri Kimlik Bilgisi',
+                                            'Hizmet No',
+                                            'Okul'])
+                df_mys = df_mys[['Fatura No','Ödenecek Tutar','Tarih','Abone','VKN']]
+
+                df_mebbis = pd.read_excel(yolFatura, header=0) #mebbis dosyasından verileri çekiyor
+
+                df_mebbis['KURUM ADI'] = 'İlçe Milli Eğitim Müdürlüğü'
+                df_mebbis['VERGİ NO'] = 7820458686
+                df_mebbis = df_mebbis.rename(columns={'İCMAL NUMARASI':'İCMAL NO'})
+
+                cols_to_keep_meb = [
+                    'KURUM ADI', #eklenecek
+                    'ABONE NUMARASI', 
+                    'VERGİ NO', #eklenecek
+                    'FATURA NUMARASI', 
+                    'FATURA TARİHİ',
+                    'İCMAL NO',
+                    'TÜKETİM MİKTARI',
+                    'FATURA TUTARI'
+                ]
+                df_mebbis = df_mebbis.reindex(cols_to_keep_meb, axis=1)
+                df_mebbis = df_mebbis.drop(df_mebbis.index[-1]) #tablodaki son satırı siliyor
+                #df_mebbis['ABONE NUMARASI']=df_mebbis['ABONE NUMARASI'].astype(int) #abone numaralarını mys dosyasıyla eşlemek için tam sayı haline getiriyor
+                df_mebbis['VERGİ NO']=df_mebbis['VERGİ NO'].astype(str) #ilerde hata almamak için kolonun veri tipini değiştiriyor
+                df_mebbis['İCMAL NO']=df_mebbis['İCMAL NO'].astype(str) #böylece normalde sayısal veriler kayıpsız şekilde metin olarak saklanabiliyor
+                
+                df_mebbis_dummy = pd.read_excel(yolFatura, header=0, dtype=str) #aynı mebbis dosyasını okuyor ancak vergi no ve icmal metin olarak saklanmalı
+                df_mebbis_dummy['VERGİ NO'] = '7820458686'
+                df_mebbis_dummy = df_mebbis_dummy.rename(columns={'İCMAL NUMARASI':'İCMAL NO'})
+                cols_to_keep_dummy = ['VERGİ NO','İCMAL NO'] #bu veriler sayısal olarak saklanırsa başında 0 varsa siliyor
+                df_mebbis_dummy = df_mebbis_dummy.reindex(cols_to_keep_dummy, axis=1)
+                df_mebbis_dummy = df_mebbis_dummy.drop(df_mebbis_dummy.index[-1])    
+
+                df_kurum = pd.read_excel(yolKurum, dtype=str, header=0) #kurumlar dosyasından verileri çekiyor
+                cols_to_keep_kurum = [
+                            'VERGİ KİMLİK NO', 
+                            'KURUM TÜRÜ'
+                        ]
+                df_kurum = df_kurum.reindex(cols_to_keep_kurum, axis=1)
+                df_kurum =df_kurum.drop(df_kurum.index[-1])
+                df_kurum_ana = df_kurum[df_kurum['KURUM TÜRÜ']=='Okul Öncesi'] #anaokullarını ayırıyor
+                df_kurum_mem = df_kurum[df_kurum['KURUM TÜRÜ']=='MEM'] #MEM'i ayırıyor
+                
+                #Tek kaynak formu işlemleri
+                toplam = "{:,.2f}".format(df_mebbis['FATURA TUTARI'].sum())
+                toplam = toplam.replace(',','-')
+                toplam = toplam.replace('.',',')
+                toplam = toplam.replace('-','.')
+                top2 = df_mebbis['FATURA TUTARI'].sum()
+                tutar = f'{toplam} TL ({ParaCevir(top2)})'
+                unik = len(df_mebbis['KURUM ADI'].unique())
+                if unik<4:
+                    kurumlar = ", ".join(str(element) for element in df_mebbis['KURUM ADI'].unique())
+                else:
+                    kurumlar = f'{len(df_mebbis)} Adet Fatura'
+                icmaller = ", ".join(str(element) for element in df_mebbis_dummy['İCMAL NO'].unique())
+                ihtiyaç = f'İLÇE MİLLİ EĞİTİM MÜDÜRLÜĞÜ {self.dönem} DÖNEM TELEFON ÖDEMESİ (İcmal No: {icmaller})'
+                tekKaynak = {
+                    'firma' : df_firma.values[0][1],
+                    'tebligat' : df_firma.values[1][1],
+                    'vergi' : df_firma.values[2][1],
+                    'telefon' : df_firma.values[3][1],
+                    'eposta' : df_firma.values[4][1],
+                    'tutar' : tutar,
+                    'ihtiyaç' : ihtiyaç,
+                    'harcama' : imzaListe[2],
+                    'unvan' : imzaListe[3]
+                }
+                
+                tür = 'telefon'
+                ilkTertip = f'40.149.423.292.13.68.01.03.05'
+                anaTertip = f'40.149.422.291.13.68.01.03.05'
+                memTertip = f'98.900.9006.306.13.67.01.03.05'
+                nitelik = f'İlçe Milli Eğitim Müdürlüğü {self.dönem} Dönem Telefon Ödemesi' ###
+                metin = f'      Müdürlüğümüze ait {self.dönem} dönem {tür} abonelikleri toplam {tutar} borç ödemesi hususunu Onaylarınıza arz ederim.'
+                
+                harcamaTalimatı = {
+                    'tarih' : date.today().strftime("%d.%m.%Y"),
+                    'tanım' : faturaTür,
+                    'nitelik' : nitelik,
+                    'miktar' : tutar,
+                    'ödenek1' : '',
+                    'ödenek2' : '',
+                    'metin' : metin,
+                    'ilkTertip': ilkTertip,
+                    'anaTertip': anaTertip,
+                    'memTertip': memTertip
+                }       
 
                 MYS(df_mys,df_mebbis,df_mebbis_dummy, df_kurum_ana, df_kurum_mem, firma, faturaTür, imzaListe, dosyaYolu, tekKaynak, harcamaTalimatı).MYS()
 
--- mySplitGUI.py
+++ mySplitGUI.py
@@ -59,7 +59,7 @@ class MainWindow(QWidget):
         self.close()
         
     def telefonSender(self):
-        self.w = Telefon()
+        self.w = SecTelefon()
         self.w.show()
         self.close()
         
@@ -372,7 +372,7 @@ class Secİnternet(QWidget):
 
         btnMem = QPushButton(text="İlçe Milli Eğitim")
         btnMem.setFixedSize(230, 60)
-        btnMem.clicked.connect(lambda: self.admSender())
+        btnMem.clicked.connect(lambda: self.memSender())
         
 
         btnGeri = QPushButton(text='Geri Dön', parent=self)
@@ -395,7 +395,7 @@ class Secİnternet(QWidget):
         self.w.show()
         self.close()
 
-    def admSender(self):
+    def memSender(self):
         self.w = MEMİnternet()
         self.w.show()
         self.close()        
@@ -459,6 +459,110 @@ class Telefon(QWidget):
         msgBox.setIcon(QMessageBox.Icon.Information)
 
         returnValue = msgBox.exec()
+
+class MEMTelefon(QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("LETA Fatura")
+        self.setWindowIcon(QIcon('logo.png'))
+        self.setFixedSize(250,180)
+
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        
+        self.label1 = QLabel('Ay:',self) 
+        self.label1.setGeometry(15, 0, 110, 40)
+        self.label2 = QLabel('Yıl:',self)
+        self.label2.setGeometry(125, 0, 110, 40)
+        
+        self.spinAy = QSpinBox(self)
+        self.spinAy.setGeometry(10, 30, 110, 40)
+        self.spinAy.setMinimum(1)
+        self.spinAy.setMaximum(12)
+        self.spinAy.setValue(datetime.now().month-1)
+        
+        self.spinYıl = QSpinBox(self)
+        self.spinYıl.setGeometry(120, 30, 110, 40)
+        self.spinYıl.setMinimum(2017)
+        self.spinYıl.setMaximum(2027)
+        self.spinYıl.setValue(datetime.now().year)
+          
+        btnHesapla = QPushButton(text='Hesapla', parent=self)
+        btnHesapla.setFixedSize(230, 30)
+        btnHesapla.setStyleSheet("background-color : #57f752")
+        btnHesapla.move(10,80)
+        btnHesapla.clicked.connect(lambda: self.hesapla())        
+        
+        btnGeri = QPushButton(text='Geri Dön', parent=self)
+        btnGeri.setFixedSize(230, 30)
+        btnGeri.setStyleSheet("background-color : #c8d3fa")
+        btnGeri.move(10,130)
+        btnGeri.clicked.connect(lambda: self.the_back_button_was_clicked())
+
+    
+    def the_back_button_was_clicked(self):
+        self.w = Secİnternet()
+        self.w.show()
+        self.close()      
+        
+    def hesapla(self):
+        Fatura(self.spinAy.value(),self.spinYıl.value()).MEMTelefon()
+        self.showDialog()
+
+    def showDialog(self):
+        msgBox = QMessageBox(self)
+        msgBox.setText("Hesaplama tamamlandı!")
+        msgBox.setWindowTitle("LETA Fatura")
+        msgBox.setStandardButtons(QMessageBox.StandardButton.Ok)
+        msgBox.setIcon(QMessageBox.Icon.Information)
+
+        returnValue = msgBox.exec()
+
+class SecTelefon(QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("LETA İnternet")
+        self.setWindowIcon(QIcon('logo.png'))
+        self.setFixedSize(250,180)
+
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        
+        btnTem = QPushButton(text="Temel Eğitim")
+        btnTem.setFixedSize(230, 60)
+        btnTem.clicked.connect(lambda: self.telefonSender())
+
+        btnMem = QPushButton(text="İlçe Milli Eğitim")
+        btnMem.setFixedSize(230, 60)
+        btnMem.clicked.connect(lambda: self.memSender())
+        
+
+        btnGeri = QPushButton(text='Geri Dön', parent=self)
+        btnGeri.setFixedSize(230, 30)
+        btnGeri.setStyleSheet("background-color : #c8d3fa")
+        btnGeri.clicked.connect(lambda: self.the_back_button_was_clicked())
+
+        layout.addWidget(btnTem)
+        layout.addWidget(btnMem)
+        layout.addWidget(btnGeri)
+
+    
+    def the_back_button_was_clicked(self):
+        self.w = MainWindow()
+        self.w.show()
+        self.close()      
+
+    def telefonSender(self):
+        self.w = Telefon()
+        self.w.show()
+        self.close()
+
+    def memSender(self):
+        self.w = MEMTelefon()
+        self.w.show()
+        self.close()      
 
 class Doğalgaz(QWidget):
     def __init__(self):
